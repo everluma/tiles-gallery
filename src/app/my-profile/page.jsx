8629be103ee9abb97bc4e2b2706023e445e98cde
everluma <farjanaaktarmonisha@gmail.com>
2026-05-11 14:38:24 +0600

    profile page added

---
+++ src/app/my-profile/page.jsx
@@ -0,0 +1,20 @@
+const MyProfile = () => {
+  return (
+    <div className="p-10 text-center">
+      <img
+        src="https://i.ibb.co/ZYW3VTp/brown-brim.png"
+        className="w-32 h-32 rounded-full mx-auto"
+      />
+
+      <h1 className="text-4xl font-bold mt-5">
+        Monisha
+      </h1>
+
+      <button className="btn btn-primary mt-5">
+        Update Information
+      </button>
+    </div>
+  );
+};
+
+export default MyProfile;
